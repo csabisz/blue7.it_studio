70623b96636478c3593ed5c5c5ee60713c9b0bbd
Csabi Szekely <89836007+csabisz@users.noreply.github.com>
2026-02-18 16:32:04 +0200

    background color modal

--- ajax/ajax_customer_files.php
+++ ajax/ajax_customer_files.php
@@ -41,7 +41,7 @@ $o_id=$prod->xss_fix($_GET['o_id']);
 			<button type="button" id="choose_customer_files_btn" data-toggle="modal" data-target="#choose_customer_files_modal" data-backdrop="static" data-keyboard="false" class="btn btn-sm btn-warning">Choose customer files...</button>
 		</div>
 		<!-- Modal -->
-		<div class="modal fade" id="choose_customer_files_modal" tabindex="-1" role="dialog" aria-labelledby="choose_customer_filesLabel" aria-hidden="true">
+		<div class="modal fade" id="choose_customer_files_modal" tabindex="-1" role="dialog" aria-labelledby="choose_customer_filesLabel" aria-hidden="true" style="background-color:#C4A484;">
 		<div class="modal-dialog modal-lg" role="document">
 			<div class="modal-content">
 			<div class="modal-header">
