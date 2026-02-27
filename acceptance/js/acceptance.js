29367dbd96ac88a49e30b94144edbaad90e910a7
csabisz <csabi.szekely@yahoo.com>
2026-02-27 18:27:48 +0200

    col_amount0 change

--- acceptance/js/acceptance.js
+++ acceptance/js/acceptance.js
@@ -691,6 +691,7 @@ $(document).ready(function() {
 	$('#col_amount1_in_b1').on('change keyup paste mouseup', function() {
 		$('#col_amount2_in_b1').val($('#col_amount1_in_b1').val());
 		$('#col_amount3_in_b1').val($('#col_amount1_in_b1').val());
+		$('#col_amount0').val($('#col_amount1_in_b1').val());
 		calculatePricesAPUslabcs_in_b1();
 		calcPurchaserPrice_in_b1();
 		calcProducerPrice_in_b1();
@@ -701,6 +702,7 @@ $(document).ready(function() {
 	$('#col_amount2_in_b1').on('change keyup paste mouseup', function() {
 		$('#col_amount1_in_b1').val($('#col_amount2_in_b1').val());
 		$('#col_amount3_in_b1').val($('#col_amount2_in_b1').val());
+		$('#col_amount0').val($('#col_amount2_in_b1').val());
 		calculatePricesAPUslabcs_in_b1();
 		calcPurchaserPrice_in_b1();
 		calcProducerPrice_in_b1();
@@ -711,6 +713,7 @@ $(document).ready(function() {
 	$('#col_amount3_in_b1').on('change keyup paste mouseup', function() {
 		$('#col_amount1_in_b1').val($('#col_amount3_in_b1').val());
 		$('#col_amount2_in_b1').val($('#col_amount3_in_b1').val());
+		$('#col_amount0').val($('#col_amount3_in_b1').val());
 		calculatePricesAPUslabcs_in_b1();
 		calcPurchaserPrice_in_b1();
 		calcProducerPrice_in_b1();
