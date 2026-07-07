d6294b0f96055aadf7adf3872ae2260a508c8ddf
Csabi Sz <csabisz13@gmail.com>
2026-07-07 20:32:47 +0300

    not empty check

--- ajax/update_client_rights_status.php
+++ ajax/update_client_rights_status.php
@@ -8,7 +8,7 @@ $data['u_status']=$prod->xss_fix($_POST['u_status']);
 
 $client_rights=$prod->get_client_rights($data['client_id']);
 
-if(count($client_rights)>0)
+if(!empty($client_rights))
 {
     $prod->update_client_rights_status(json_encode($data));
 }
