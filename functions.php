424ea41a852d5cc0c71f9dc0d4bb7b98215f4915
Csabi Sz <csabisz13@gmail.com>
2026-07-25 15:17:46 +0300

    middle name show v1

--- functions.php
+++ functions.php
@@ -5918,7 +5918,7 @@ class Production
 
         //$stmt=mysqli_prepare($mysqli,"select * from `u_creators` where `lt_id`=? order by `lt_id`,`uca_name` asc");
 //        $stmt = mysqli_prepare($mysqli, "select `u_clients`.`client_ID`, `c_first_name`, `c_last_name`, `l_first_name`, `l_last_name` from `u_clients` join `u_clients_rights` on `u_clients`.`client_ID`=`u_clients_rights`.`client_id` where `u_clients`.`lt_id`=? and `u_clients_rights`.`u_status`='active' order by `u_clients`.`c_first_name` asc");
-        $stmt = mysqli_prepare($mysqli, "select `u_clients`.`client_ID`, `c_first_name`, `c_last_name` from `u_clients` join `u_clients_rights` on `u_clients`.`client_ID`=`u_clients_rights`.`client_id` where `u_clients`.`lt_id`=? and `u_clients_rights`.`u_status`='active' order by `u_clients`.`c_first_name` asc");
+        $stmt = mysqli_prepare($mysqli, "select `u_clients`.`client_ID`, `c_first_name`, `c_middle_name`, `c_last_name` from `u_clients` join `u_clients_rights` on `u_clients`.`client_ID`=`u_clients_rights`.`client_id` where `u_clients`.`lt_id`=? and `u_clients_rights`.`u_status`='active' order by `u_clients`.`c_first_name` asc");
 
         mysqli_stmt_bind_param($stmt, "i", $lt_id);
 
