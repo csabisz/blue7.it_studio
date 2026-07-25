1e3bb03851e371c4b8256a6b8d7e80b0a2efeba9
Csabi Sz <csabisz13@gmail.com>
2026-07-25 19:01:35 +0300

    qualify right check

--- ajax/load_all_creators.php
+++ ajax/load_all_creators.php
@@ -14,35 +14,43 @@ $all_other_creators = $prod->show_creators_other_companies($lt_id);
 <?php
 for($c=0;$c<count($all_creators);$c++)
 {
-    ?>
-    <option value="<?php echo $all_creators[$c]['client_ID'];?>"><?php  
-    $creator_name = $all_creators[$c]['c_first_name'];
-    if(!empty($all_creators[$c]['c_middle_name'])) $creator_name .= ' ' . $all_creators[$c]['c_middle_name'];
-    $creator_name .= ' ' . $all_creators[$c]['c_last_name'];
+    $client_rights = $prod->get_client_rights($all_creators[$c]['client_ID']);
+    if($client_rights['qualified_for_all_tasks']>0)
+    {
+        ?>
+        <option value="<?php echo $all_creators[$c]['client_ID'];?>"><?php  
+        $creator_name = $all_creators[$c]['c_first_name'];
+        if(!empty($all_creators[$c]['c_middle_name'])) $creator_name .= ' ' . $all_creators[$c]['c_middle_name'];
+        $creator_name .= ' ' . $all_creators[$c]['c_last_name'];
 
-    echo $creator_name;
+        echo $creator_name;
 
-    $company_name = $prod->get_company($all_creators[$c]['lt_id']);
-    echo " - ".$company_name['mailnick'];
-    ?></option>
-<?php
+        $company_name = $prod->get_company($all_creators[$c]['lt_id']);
+        echo " - ".$company_name['mailnick'];
+        ?></option>
+        <?php
+    }
 }
 ?>
 <option disabled="" style="font-weight: bold; background-color: grey; color: white;">Other Companies</option>
 <?php
 for($c=0;$c<count($all_other_creators);$c++)
 {
-    ?>
-    <option value="<?php echo $all_other_creators[$c]['client_ID'];?>"><?php  
-    $creator_name = $all_other_creators[$c]['c_first_name'];
-    if(!empty($all_other_creators[$c]['c_middle_name'])) $creator_name .= ' ' . $all_other_creators[$c]['c_middle_name'];
-    $creator_name .= ' ' . $all_other_creators[$c]['c_last_name'];
+    $client_rights = $prod->get_client_rights($all_other_creators[$c]['client_ID']);
+    if($client_rights['qualified_for_all_tasks']>0)
+    {
+        ?>
+        <option value="<?php echo $all_other_creators[$c]['client_ID'];?>"><?php  
+        $creator_name = $all_other_creators[$c]['c_first_name'];
+        if(!empty($all_other_creators[$c]['c_middle_name'])) $creator_name .= ' ' . $all_other_creators[$c]['c_middle_name'];
+        $creator_name .= ' ' . $all_other_creators[$c]['c_last_name'];
 
-    echo $creator_name;
+        echo $creator_name;
 
-    $company_name = $prod->get_company($all_other_creators[$c]['lt_id']);
-    echo " - ".$company_name['mailnick'];
-    ?></option>
-<?php
+        $company_name = $prod->get_company($all_other_creators[$c]['lt_id']);
+        echo " - ".$company_name['mailnick'];
+        ?></option>
+        <?php
+    }
 }
 ?>
