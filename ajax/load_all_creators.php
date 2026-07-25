bb59e64a5d243e2acef8b65c7569f8f74f5b8723
Csabi Sz <csabisz13@gmail.com>
2026-07-25 17:03:36 +0300

    load creators

--- ajax/load_all_creators.php
+++ ajax/load_all_creators.php
@@ -9,6 +9,34 @@ $lt_id=$prod->xss_fix($_GET['lt_id']);
 $all_creators = $prod->show_creators($lt_id);
 
 $all_other_creators = $prod->show_creators_other_companies($lt_id);
+?>
+<option style="font-weight: bold; background-color: grey; color: white;">-- Choose creator --</option>
+<?php
+for($c=0;$c<count($all_creators);$c++)
+{
+    ?>
+    <option value="<?php echo $all_creators['client_ID'];?>"><?php  
+    $creator_name = $all_creators['c_first_name'];
+    if(!empty($all_creators['c_middle_name'])) $creator_name .= ' ' . $all_creators['c_middle_name'];
+    $creator_name .= ' ' . $creator['c_last_name'];
 
-print_r($all_creators);
+    echo $creator_name;
+    ?></option>
+<?php
+}
+?>
+<option disabled="" style="font-weight: bold; background-color: grey; color: white;">Other Companies</option>
+<?php
+for($c=0;$c<count($all_other_creators);$c++)
+{
+    ?>
+    <option value="<?php echo $all_other_creators['client_ID'];?>"><?php  
+    $creator_name = $all_other_creators['c_first_name'];
+    if(!empty($all_other_creators['c_middle_name'])) $creator_name .= ' ' . $all_other_creators['c_middle_name'];
+    $creator_name .= ' ' . $creator['c_last_name'];
+
+    echo $creator_name;
+    ?></option>
+<?php
+}
 ?>
